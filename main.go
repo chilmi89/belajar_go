86e83f2e960a916df47752eb1150d06b9070b041
shynix89 <achmadchilmi87@gmail.com>
2026-02-10 00:41:01 +0700

    commit ke 4

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"belajar_golang/config"
 	"belajar_golang/controllers"
+	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,6 +15,12 @@ func main() {
 	r := gin.Default()
 	r.GET("/users", controllers.AmbilDataUser)
 
+	// Ambil port dari environment variable, default 8080
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
-	r.Run("localhost:8080")
+	log.Println("Server running on port", port)
+	r.Run(":" + port) // bind ke semua interface
 }
